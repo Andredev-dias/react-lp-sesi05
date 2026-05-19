1c75f2a36a6d22a085d915d164f4fc1da52d4fd6
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-05-19 08:40:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import s from './App.module.css'
+
+
+function App() {
+
+//js
+
+  return (
+      <h1 className={s.nomeDaCLasse}>meu primeiro react com vite</h1>
+  )
+}
+
+export default App
+
